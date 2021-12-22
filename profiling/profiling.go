d8e976538f5d8573d89e915801801f6a1da38c77
WuZheng <jenniferwzz@qq.com>
2021-12-22 22:43:25 +0800

    emmmm

---
+++ profiling/profiling.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+// go tool pprof profiling profiling.prof
+func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+	// 假装做一些事情
+	for i := 0; i < 10000; i++ {
+		i := i
+		go fmt.Printf("do something: %d", i)
+	}
+}
